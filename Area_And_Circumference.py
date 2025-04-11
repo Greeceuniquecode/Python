e4c60dc76663d67740b6a53d7d10907c913953c3
Greeceuniquecode <notgreece2@gmail.com>
2025-04-11 12:57:27 +0545

    Python basics

---
+++ Area_And_Circumference.py
@@ -0,0 +1,6 @@
+# Created by Greece Dahal
+radius = float(input("Enter the radius of circle\n"))
+area = (22/7)*radius**2
+circumference = 2*(22/7)*radius
+print("The area of given circle is: ", area)
+print("The circumference of given circle is: ", circumference)
---
+++ Sum_of_n_number.py
@@ -0,0 +1,10 @@
+#created by greeceuniquecode
+i = 0
+sum = 0
+size = int(input("How many numbers do you want to add?\n"))
+print("Enter the number that you want to add.")
+while (i < size):
+    number = int(input())
+    sum = sum+number
+    i = i+1
+print("The sum is:  ", sum)

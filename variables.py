50bcaeaa3fabfe72a906d73b15284b34ff4274fb
Greeceuniquecode <notgreece2@gmail.com>
2025-07-28 18:12:32 +0545

    Variables in python

---
+++ variables.py
@@ -0,0 +1,9 @@
+#python is dynamically typed language means you don't need to declare the type of variable before using it
+intVariable=1
+floatVariable=2.55
+stringVariable="hello world"
+print(intVariable)
+print(floatVariable)
+print(stringVariable)
+#for concatinating 
+print(stringVariable+" Concatinated" )

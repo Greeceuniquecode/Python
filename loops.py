a34079d6c8eaea254e0495df6f93dea6982de22f
Greeceuniquecode <notgreece2@gmail.com>
2025-07-29 18:45:46 +0545

    Loops in Python

---
+++ loops.py
@@ -0,0 +1,15 @@
+# For loop
+for i in range (1,11) :
+    print(i, end=" ")
+print("\n")
+# while loop 
+a=1
+while a<=10 :
+    print(a,end=" ")
+    a+=1
+print("\n")
+
+for i in range (1,4) :
+    for j in range (1,4) :
+        print(j,end="\t")
+    print()

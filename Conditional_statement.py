329df629f5eae983b1889e3f51825c88007e5f5a
Greeceuniquecode <notgreece2@gmail.com>
2025-07-29 18:24:30 +0545

    Variables and Conditional statements in Python

---
+++ Conditional_statement.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter a number.\n"))
+
+if num1==0 :
+    print(num1, " is Zero")
+else:
+    if num1%2==0 :
+        print(num1," is Even.")
+    else :
+        print(num1, " is Odd.")
+
+    if num1>0 :
+        print(num1," is positive.")
+    else :
+        print(num1," is Negative")
